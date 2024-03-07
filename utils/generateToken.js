5a845a2fd48d7a21583a3b42096c7c061d762bb7
iSRHcoder <s.r.hatkadke@gmail.com>
2024-03-07 21:58:24 +0530

    NODE_ENV changed

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -4,12 +4,12 @@ const generateTokenAndSetCookie = (userId, res) => {
   const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
     expiresIn: '15d',
   });
-  console.log('process.env.NODE_ENV', process.env.NODE_ENV);
+
   res.cookie('jwt', token, {
     maxAge: 15 * 24 * 60 * 60 * 1000, //should be in milliSeconds
     httpOnly: true, //prevent XSS attacks cross-site scripting attacks
     sameSite: 'strict', // CSRF attacks cross-site request forgery attacks
-    secure: process.env.NODE_ENV === 'development' ? true : false,
+    secure: process.env.NODE_ENV !== 'development',
   });
 };
 
