860ee8bc45527f567abc7b2459b475c3e7fd10df
iSRHcoder <s.r.hatkadke@gmail.com>
2024-03-07 11:01:12 +0530

    removed unwanted comments and refactored code

--- index.js
+++ index.js
@@ -6,18 +6,16 @@ import userRoutes from './routes/userRoutes.js';
 import connectToMongoDb from './db/connectToMongoDB.js';
 import cookieParser from 'cookie-parser';
 import { app, server } from './socket/socket.js';
-// const bodyParser = require('body-parser');
-// const cors = require('cors');
+import bodyParser from 'body-parser';
+import cors from 'cors';
 
 dotenv.config();
 
-app.use(express.json()); // to parse the incoming requests with JSON payloads(from req.body)
+app.use(express.json());
 app.use(cookieParser());
-// app.use(bodyParser.json());
-// app.use(cors());
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
-});
+app.use(bodyParser.json());
+app.use(cors());
+
 app.use('/api/auth', authRoutes);
 app.use('/api/messages', messageRoutes);
 app.use('/api/users', userRoutes);
