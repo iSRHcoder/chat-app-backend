aecfbacdc78aa7f113c766469354a23ad569ddb7
iSRHcoder <s.r.hatkadke@gmail.com>
2024-03-07 21:55:34 +0530

    .

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -1,10 +1,10 @@
 import jwt from 'jsonwebtoken';
 
 const generateTokenAndSetCookie = (userId, res) => {
-  console.log('process.env.JWT_SECRET', process.env.JWT_SECRET);
   const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
     expiresIn: '15d',
   });
+  console.log('process.env.NODE_ENV', process.env.NODE_ENV);
   res.cookie('jwt', token, {
     maxAge: 15 * 24 * 60 * 60 * 1000, //should be in milliSeconds
     httpOnly: true, //prevent XSS attacks cross-site scripting attacks
