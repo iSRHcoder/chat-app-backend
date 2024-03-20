f62ac03f7bb0b94a11e933d7d74053e6de8bbe1d
iSRHcoder <s.r.hatkadke@gmail.com>
2024-03-20 21:30:35 +0530

    console.log statement deleted

--- middleware/protectRoute.js
+++ middleware/protectRoute.js
@@ -4,7 +4,6 @@ import User from '../models/userModel.js';
 const protectRoute = async (req, res, next) => {
   try {
     const token = req.cookies.jwt;
-    console.log('req.cookies', req.cookies);
     if (!token) {
       return res.status(401).json({
         status: 'failed',
